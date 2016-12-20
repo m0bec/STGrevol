957afb452da79a98ec43f6ce9e6cbf00cec22100
mobec <mobec622@outlook.jp>
2016-12-20 21:03:46 +0900

    画面を表示させる
    画面サイズは変更する予定

---
+++ STGrevol/main.cpp
@@ -0,0 +1,32 @@
+#include "DxLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(true);
+	// 画面モードの設定
+	SetGraphMode(960, 720, 16);
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1) return -1;
+
+	// グラフィックの描画先を裏画面にセット
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	while (1) {
+		// 画面を初期化(真っ黒にする)
+		ClearDrawScreen();
+
+		// 裏画面の内容を表画面にコピーする
+		ScreenFlip();
+
+		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
+		// -1 が返ってきたらループを抜ける
+		if (ProcessMessage() < 0) break;
+
+		// もしＥＳＣキーが押されていたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
+	}
+
+	DxLib_End();
+	return 0;
+}
