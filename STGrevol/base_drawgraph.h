3f123ba457183fa813928c9f4fe02d613840bfb3
mobec <mobec622@outlook.jp>
2017-01-04 18:47:18 +0900

    画像描写のスーパークラスの変数＆関数を準備

--- STGrevol/base_drawgraph.h
+++ STGrevol/base_drawgraph.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "DxLib.h"
+#include <string>
+
+class Base_drawgraph {
+public:
+
+	Base_drawgraph(double *define_srsize_, int *gr_);
+private:
+	double *define_srsize;
+	int *gr;
+
+	void Drawgr();
+};
