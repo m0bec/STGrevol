4bffef1192a03fb886829aa96e85a559ca906824
mobec <mobec622@outlook.jp>
2017-01-04 18:39:43 +0900

    ゲッターの関数の中身を作成

--- STGrevol/loadgr.h
+++ STGrevol/loadgr.h
@@ -4,9 +4,9 @@
 class Loadgr {
 public:
 	Loadgr();
-	int titlegr();
-	int startgr();
-	int exitgr();
+	int Titlegr() { return titlegr; }
+	int Startgr() { return startgr; }
+	int Exitgr() { return exitgr; }
 
 private:
 	int titlegr;
