a3c2385a97e298c05081d36e91a69a4629fbef30
mobec <mobec622@outlook.jp>
2017-01-04 20:11:24 +0900

    画像のロードを非同期読み込みに変更

--- STGrevol/loadgr.cpp
+++ STGrevol/loadgr.cpp
@@ -1,7 +1,9 @@
 #include "loadgr.h"
 
 void Loadgr::Synchronizing_loadgr() {
+	SetUseASyncLoadFlag(true);
 	titlegr = LoadGraph("Graph/title.png");
 	startgr = LoadGraph("Graph/start.png");
 	exitgr = LoadGraph("Graph/exitgr.png");
+	SetUseASyncLoadFlag(false);
 }
