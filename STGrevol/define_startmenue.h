a9e920a54bc0c0f8ac0b8f7ae1cfaea784658372
mobec <mobec622@outlook.jp>
2017-01-05 23:37:24 +0900

    メニューの番号をdefineで定義

--- STGrevol/define_startmenue.h
+++ STGrevol/define_startmenue.h
@@ -1,5 +1,9 @@
 #pragma once
 #define MAX_MENUE_NUMBER 4	//ÉÅÉjÉÖÅ[ÇÃêîÅ@
+#define START_MENUE_NUMBER 0
+#define EXIT_MENUE_NUMBER 3
+#define MANUAL_MENUE_NUMBER 1
+#define KEYCON_MENUE_NUMBER 2
 #define MOVE_MENUE_DIST 100
 #define TITLEMEN_BASIC_PLACE_Y 300
 #define TITLEMEN_BASIC_PLACE_X 220
