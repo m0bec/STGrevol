2b2791cbef128fa55992bea7e8972841e4a1fb12
mobec <mobec622@outlook.jp>
2017-01-06 14:52:09 +0900

    ゲームパッドの押したボタンを表示する関数を準備

--- STGrevol/keyconmode_select.cpp
+++ STGrevol/keyconmode_select.cpp
@@ -4,10 +4,11 @@ void Keyconmode_select::Getinput_param() {
 	input_pad = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 }
 
-int Keyconmode_select::Judgeinput_param() {
+void Keyconmode_select::Judgeinput_param() {
 	for (int i = 0; i < 28; i++) {
 		if (input_pad & (1 << i)) {
-			return i;
+			str_input_pad[keycon_select_var] = input_pad;
+			break;
 		}
 	}
 }
@@ -25,5 +26,98 @@ void Keyconmode_select::Keycon_select() {
 	}
 	else if (keycon_select_var > KEYCON_MODE_NUM) {
 		keycon_select_var = KEYCON_MODE_NUM;
+	}
+}
+
+void Keyconmode_select::Keycon_mode() {
+	for (int i = 0; i < KEYCON_MENUE_NUMBER; i++) {
+		switch (str_input_pad[i]) {
+		case 0:
+			break;
+
+		case 1:
+			break;
+
+		case 2:
+			break;
+
+		case 3:
+			break;
+
+		case 4:
+			break;
+
+		case 5:
+			break;
+
+		case 6:
+			break;
+
+		case 7:
+			break;
+
+		case 8:
+			break;
+
+		case 9:
+			break;
+
+		case 10:
+			break;
+
+		case 11:
+			break;
+
+		case 12:
+			break;
+
+		case 13:
+			break;
+
+		case 14:
+			break;
+
+		case 15:
+			break;
+
+		case 16:
+			break;
+
+		case 17:
+			break;
+
+		case 18:
+			break;
+
+		case 19:
+			break;
+
+		case 20:
+			break;
+
+		case 21:
+			break;
+
+		case 22:
+			break;
+
+		case 23:
+			break;
+
+		case 24:
+			break;
+
+		case 25:
+			break;
+
+		case 26:
+			break;
+
+		case 27:
+			break;
+
+		case 28:
+			break;
+		}
 	}
 }
--- STGrevol/keyconmode_select.h
+++ STGrevol/keyconmode_select.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "DxLib.h"
 #include "base_draw_startmenue.h"
-#define KEYCON_MODE_NUM 5
+#include <array>
+#define KEYCON_MODE_NUM 5	//keycon_select_var == KEYCON_MDE_NUM‚ÌŽž‚ÍExit‚ð‚³‚·
 
 class Keyconmode_select : public Base_draw_startmenue {
 public:
 	using Base_draw_startmenue::Base_draw_startmenue;
 
 	void Getinput_param();
-	int Judgeinput_param();
+	void Judgeinput_param();
 	void Keycon_select();
+	void Keycon_mode();
+	
 private:
+	std::array<int, KEYCON_MENUE_NUMBER + 1> str_input_pad;
 	int input_pad = 0;
 	int keycon_select_var = 0;
 };
