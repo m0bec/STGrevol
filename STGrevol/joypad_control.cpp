ea165dea8dbb080e8631c8c6e0a4e5338e8f82ad
mobec <mobec622@outlook.jp>
2017-01-08 16:02:53 +0900

    ファイルからボタンの配置を読み込む

---
+++ STGrevol/joypad_control.cpp
@@ -0,0 +1,22 @@
+#include "joypad_control.h"
+
+Joypad_control::Joypad_control(std::string filename_) {
+	filename = filename_;
+	std::ifstream ifs(filename, std::ios_base::binary);
+	try {
+		if (ifs.fail()) {
+			throw 99;
+		}
+		ifs.read(reinterpret_cast<char*>(str_button_bin), BUTTON_NUM*sizeof(int));
+		button_shot = str_button_bin[0];
+		button_bomb = str_button_bin[1];
+		button_slow = str_button_bin[2];
+		button_rightturn = str_button_bin[3];
+		button_leftturn = str_button_bin[4];
+	}
+	catch (int error) {
+		std::cerr << "error " << error << std::endl;
+		std::exit(1);
+	}
+
+}
--- STGrevol/joypad_control.h
+++ STGrevol/joypad_control.h
@@ -1,15 +1,22 @@
 #pragma once
 #include "DxLib.h"
 #include <fstream>
+#include <iostream>
+#include <string>
+#define BUTTON_NUM 5
 
 class Joypad_control {
 public:
-	Joypad_control();
+	Joypad_control(std::string filename_);
 	void Get_keyconfig();
-
+	
 	int button_shot;
 	int button_bomb;
 	int button_slow;
 	int button_rightturn;
 	int button_leftturn;
+
+private:
+	std::string filename;
+	int str_button_bin[BUTTON_NUM];
 };
