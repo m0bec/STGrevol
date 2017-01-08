49a82b6ace03683d11d5b9cabd17c95c4a511dc6
mobec <mobec622@outlook.jp>
2017-01-08 16:07:12 +0900

    更新されたkeyを読み込む関数を追加

--- STGrevol/joypad_control.cpp
+++ STGrevol/joypad_control.cpp
@@ -18,5 +18,23 @@ Joypad_control::Joypad_control(std::string filename_) {
 		std::cerr << "error " << error << std::endl;
 		std::exit(1);
 	}
+}
 
+void Joypad_control::Get_keyconfig() {
+	std::ifstream ifs(filename, std::ios_base::binary);
+	try {
+		if (ifs.fail()) {
+			throw 99;
+		}
+		ifs.read(reinterpret_cast<char*>(str_button_bin), BUTTON_NUM * sizeof(int));
+		button_shot = str_button_bin[0];
+		button_bomb = str_button_bin[1];
+		button_slow = str_button_bin[2];
+		button_rightturn = str_button_bin[3];
+		button_leftturn = str_button_bin[4];
+	}
+	catch (int error) {
+		std::cerr << "error " << error << std::endl;
+		std::exit(1);
+	}
 }
