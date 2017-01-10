9df121ed64a9fd0a182af574dd752a52348d8fbb
mobec <mobec622@outlook.jp>
2017-01-10 23:28:56 +0900

    jpypadでshotを入力できるようになった
    たぶん他のキーも入力できてると思う

--- STGrevol/exitmode_select.h
+++ STGrevol/exitmode_select.h
@@ -4,9 +4,14 @@
 
 class Exitmode_select : public Base_draw_startmenue {
 public:
-	using Base_draw_startmenue::Base_draw_startmenue;
+	//using Base_draw_startmenue::Base_draw_startmenue;
+	Exitmode_select(double *define_srsize_, int *gr_, int draw_x_, int draw_y_, int rota_cx_, int rota_cy_, double rota_radian_, int *menue_var_, int this_menue_var_, bool *p_push_joyshot_flag_) : Base_draw_startmenue(define_srsize_, gr_, draw_x_, draw_y_, rota_cx_, rota_cy_, rota_radian_, menue_var_, this_menue_var_) {
+		p_push_joyshot_flag = p_push_joyshot_flag_;
+		exit_flag = false;
+	}
 	void Select_exit();
 	bool Exit_flag();
 private:
-	bool exit_flag = false;
+	bool exit_flag;
+	bool *p_push_joyshot_flag;
 };
--- STGrevol/joypad_control.cpp
+++ STGrevol/joypad_control.cpp
@@ -7,12 +7,11 @@ Joypad_control::Joypad_control(std::string filename_) {
 		if (ifs.fail()) {
 			throw 99;
 		}
-		ifs.read(static_cast<char*>(static_cast<void*>(str_button_bin)), BUTTON_NUM*sizeof(int));
-		button_shot = str_button_bin[0];
-		button_bomb = str_button_bin[1];
-		button_slow = str_button_bin[2];
-		button_rightturn = str_button_bin[3];
-		button_leftturn = str_button_bin[4];
+		ifs.read(static_cast<char*>(static_cast<void*>(&button_shot)), sizeof(int));
+		ifs.read(static_cast<char*>(static_cast<void*>(&button_bomb)), sizeof(int));
+		ifs.read(static_cast<char*>(static_cast<void*>(&button_slow)), sizeof(int));
+		ifs.read(static_cast<char*>(static_cast<void*>(&button_rightturn)), sizeof(int));
+		ifs.read(static_cast<char*>(static_cast<void*>(&button_leftturn)), sizeof(int));
 		shot_flag = false;
 		bomb_flag = false;
 		slow_flag = false;
@@ -26,8 +25,17 @@ Joypad_control::Joypad_control(std::string filename_) {
 	catch (int error) {
 		std::cerr << "error " << error << std::endl;
 		ifs.close();
+		button_shot = PAD_INPUT_1;
+		button_bomb = PAD_INPUT_2;
+		button_slow = PAD_INPUT_3;
+	    button_rightturn = PAD_INPUT_4;
+		button_leftturn = PAD_INPUT_5;
 		std::ofstream ofs(filename, std::ios_base::binary | std::ios_base::trunc);
-		ofs << PAD_INPUT_1 << PAD_INPUT_2 << PAD_INPUT_3 << PAD_INPUT_4 << PAD_INPUT_5;
+		ofs.write(static_cast<char*>(static_cast<void*>(&button_shot)), sizeof(button_shot));
+		ofs.write(static_cast<char*>(static_cast<void*>(&button_bomb)), sizeof(button_bomb));
+		ofs.write(static_cast<char*>(static_cast<void*>(&button_slow)), sizeof(button_slow));
+		ofs.write(static_cast<char*>(static_cast<void*>(&button_rightturn)), sizeof(button_rightturn));
+		ofs.write(static_cast<char*>(static_cast<void*>(&button_leftturn)), sizeof(button_leftturn));
 		std::exit(1);
 	}
 }
@@ -52,7 +60,7 @@ void Joypad_control::Get_keyconfig() {
 }
 
 void Joypad_control::Check_key(int button_, bool *flag_) {
-	if (GetJoypadInputState(DX_INPUT_PAD1) & button_) {
+	if ((GetJoypadInputState(DX_INPUT_PAD1) & button_) == button_) {
 		*flag_ = true;
 	}
 	else {
